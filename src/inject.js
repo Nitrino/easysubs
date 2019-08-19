d9d869547540551c84d741eb99583b5f6485f3c4
Petr Stepchenko <nitrinodev@gmail.com>
2019-08-20 01:41:20 +0200

    Add getSubs method

--- src/inject.js
+++ src/inject.js
@@ -1,29 +1,28 @@
 import { ready } from './ready.js';
 import Subtitles from './subtitles.js';
 
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+chrome.extension.sendMessage({}, function (response) {
+  var readyStateCheckInterval = setInterval(function () {
+    if (document.readyState === "complete") {
+      clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js1");
-		// ----------------------------------------------------------
+      // ----------------------------------------------------------
+      // This part of the script triggers when page is done loading
+      console.log("Hello. This message was sent from scripts/inject.js1");
+      // ----------------------------------------------------------
 
-    let eng_subs = new Subtitles("eng")
-    eng_subs.getFile()
+      Subtitles.getSubs("eng")
+        .then(function (data) {
+          console.log(data);
+        })
 
-    console.log(eng_subs)
-    console.log(eng_subs.file())
-    
-    const video = document.querySelector('video');
-    ready('video', function(element) {
-      element.ontimeupdate = (event) => {
-        console.log(element.currentTime);
-      };
-    });
-	}
+      const video = document.querySelector('video');
+      ready('video', function (element) {
+        element.ontimeupdate = (event) => {
+          console.log(element.currentTime);
+        };
+      });
+    }
   }, 1000);
 });
 
