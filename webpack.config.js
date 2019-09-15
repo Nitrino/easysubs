c3bd3244808207d0ee5110a7a5d97e7caabd7475
Petr Stepchenko <nitrinodev@gmail.com>
2019-09-15 16:46:10 +0300

    Use scss to styles

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,47 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
   entry: {
     inject: './src/inject.ts',
-    background: './src/background.ts'
+    background: './src/background.ts',
+    styles: './src/styles.scss'
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx']
+    extensions: ['.ts', '.js', '.scss']
   },
   module: {
     rules: [{
-      use: 'ts-loader',
-      exclude: /node_modules/
-    },
-    {
-      test: /\.js$/,
-      use: ["source-map-loader"],
-      enforce: "pre"
-    }
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.js$/,
+        use: ["source-map-loader"],
+        enforce: "pre"
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
     ],
   },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      path: path.resolve(__dirname, 'dist')
+    }),
+  ],
 };
