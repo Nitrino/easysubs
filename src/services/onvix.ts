110b54433fcef187b0a92d3ec0b867387b35c0c9
Petr Stepchenko <nitrinodev@gmail.com>
2019-09-29 20:15:56 +0300

    Load subtitles for onvix films. Resolve #22

--- src/services/onvix.ts
+++ src/services/onvix.ts
@@ -1,25 +1,24 @@
-import { parse } from "subtitle";
+import { parse, subTitleType } from "subtitle";
 import UI from "../ui"
 
 class Onvix {
-  getSubs(language: string) {
-    const base_path = "https://ageofultron.site/api/v1/streaming/serials/"
-    const serial = window.location.pathname.split("/")[2]
+  async getSubs(language: string): Promise<subTitleType[]> {
+    const videoType = window.location.pathname.split("/")[1]
+    const base_pathname = "/api/v1/streaming/"
+    const videoId = window.location.pathname.split("/")[2]
     const stream = window.location.search.split("&")[0].split("=")[1]
     const season = window.location.search.split("&")[1]
     const episode = window.location.search.split("&")[2]
-    // https://ageofultron.site/api/v1/streaming/serials/046cae0eeec8e111/a070a81585fa7856.json?season=7&episode=1
-    const url = base_path + serial + "/" + stream + ".json?" + season + "&" + episode
 
-    return fetch(url)
-      .then((resp) => resp.json())
-      .then(function (data) {
-        const subs_url = data.media_files.vtt.find((el: { srclang: string }) => el.srclang === language).src
-        return fetch(subs_url).then((resp) => resp.text())
-      })
-      .then(function (text) {
-        return parse(text)
-      })
+    let url = `${window.location.origin}${base_pathname}${videoType}s/${videoId}/${stream}.json?`
+    if (videoType == "serial") { url = `${url}${season}&${episode}` }
+
+    const resp = await fetch(url);
+    const data = await resp.json();
+    const subs_url = data.media_files.vtt.find((el: { srclang: string }) => el.srclang === language).src;
+    const resp_1 = await fetch(subs_url);
+    const text = await resp_1.text();
+    return parse(text);
   }
 
   playerContainerElement(): HTMLElement {
