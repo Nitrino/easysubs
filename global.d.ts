3440f81cdb77c0d4ce26e828584fbe356a6012a0
Petr Stepchenko <nitrinodev@gmail.com>
2019-11-13 22:22:25 +0300

    draft

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,6 @@
 declare module '*';
 declare var showTranslation: boolean;
 declare var initializeInProgress: boolean;
+declare var isEnable: boolean;
 declare var netflix: any;
 declare var player: any;
--- src/settings.ts
+++ src/settings.ts
@@ -9,10 +9,21 @@ class Settings {
     settingNode.innerHTML = `
       ${logo}
       <div class="easysubs-settings-content">
-
+        <label class="easysubs-label">
+          <div class="toggle">
+            <input class="toggle-state setting-toggle" type="checkbox" name="check" value="check" ${window.isEnable ? "checked" : ""}/>
+            <div class="toggle-inner">
+              <div class="indicator"></div>
+            </div>
+            <div class="active-bg"></div>
+          </div>
+          <div class="easysubs-label-text">${window.isEnable ? "On" : "Off"}</div>
+        </label>
       </div>
     `
     parentNode.insertBefore(settingNode, referenceNode);
+
+    const settingToggleInput = document.querySelector("setting-toggle")
   }
 }
 
