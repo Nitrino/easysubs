dc1c3f52b6f9c8c56006273b6d674b78531aad68
Petr Stepchenko <nitrinodev@gmail.com>
2019-11-28 16:52:57 +0300

    Clean inject module

--- src/inject.ts
+++ src/inject.ts
@@ -1,15 +1,10 @@
 import { subTitleType } from "subtitle";
 import { updateSubs } from "./event";
 import { subsStore } from "./store";
-
-import KinoPub from "./services/kinopub";
-import Netflix from "./services/netflix";
-import Onvix from "./services/onvix";
-import YouTube from "./services/youtube";
 import UI from "./ui";
 import Utils from "./utils";
 
-const service = Utils.detectService() as KinoPub;
+const service = Utils.detectService();
 if (service) {
   console.log("EasySubs initialized. Service:", service.constructor.name);
 
@@ -19,35 +14,14 @@ if (service) {
     window.addEventListener("easysubsSubtitlesChanged", (event: any) => {
       UI.renderSettings(service.settingSelector());
       console.log("TCL: EVENT", "easysubsSubtitlesChanged");
+      console.log("Language:", event.detail);
 
-      const label = event.detail;
-      console.log(label);
-
-      // if (!label) {
-      //   UI.createSubsElement(playerContainerElement)
-      //   // .textContent = "";
-      //   UI.createSubsProgressBarElement(playerContainerElement);
-      //   return;
-      // }
-      // initialize(service, videoElement);
-
-      // function initialize(service: YouTube | Netflix | Onvix | KinoPub, videoElement: HTMLVideoElement) {
       UI.renderSubs(service.playerContainerSelector());
       UI.renderProgressBar(service.playerContainerSelector());
-      // ReactDOM.render(<SubsComponent />, subsContainerElement);
-      // const subsProgressBarElement = UI.createSubsProgressBarElement(playerContainerElement);
-      // subsElement.textContent = ""; // Clear subs loading text
 
-      service.getSubs(label).then(subs => {
+      service.getSubs(event.detail).then(subs => {
         updateSubs(subs);
-
-        //     console.log("Subtitles loaded. subs count: " + subs.length)
-        //     if (eventsHandler) { eventsHandler.removeEvents(); }
-        //     eventsHandler = new EventsHandlers(videoElement, subs, subsElement, subsProgressBarElement)
-        //     eventsHandler.addEvents();
-        //     window.initializeInProgress = false
       });
-      // }
     });
   });
   service.init();
