aa71fa41381ac52bacc6bdb779cc73318d1c9e29
Petr Stepchenko <nitrinodev@gmail.com>
2019-12-08 18:40:53 +0300

    Add sentry errors monitoring

--- src/inject.ts
+++ src/inject.ts
@@ -1,30 +1,40 @@
+import * as Sentry from "@sentry/browser";
 import { subTitleType } from "subtitle";
 import { updateSubs } from "./event";
 import { subsStore } from "./store";
 import UI from "./ui";
 import Utils from "./utils";
 
-const service = Utils.detectService();
-if (service) {
-  console.log("EasySubs initialized. Service:", service.constructor.name);
+(Sentry as any).init({ dsn: "https://f0696dfa1f80424f9f0f628d8d1d7796@sentry.io/1849876" });
 
-  window.addEventListener("easysubsVideoReady", () => {
-    console.log("TCL: EVENT", "easysubsVideoReady");
+window.addEventListener("unhandledrejection", event => {
+  Sentry.captureEvent(event.reason);
+});
 
-    window.addEventListener("easysubsSubtitlesChanged", (event: any) => {
-      UI.renderSettings(service.settingSelector());
-      console.log("TCL: EVENT", "easysubsSubtitlesChanged");
-      console.log("Language:", event.detail);
+try {
+  const service = Utils.detectService();
+  if (service) {
+    console.log("EasySubs initialized. Service:", service.constructor.name);
+    window.addEventListener("easysubsVideoReady", () => {
+      console.log("TCL: EVENT", "easysubsVideoReady");
 
-      UI.renderSubs(service.playerContainerSelector());
-      UI.renderProgressBar(service.playerContainerSelector());
-      UI.renderNotifications();
-      service.getSubs(event.detail).then(subs => {
-        updateSubs(subs);
+      window.addEventListener("easysubsSubtitlesChanged", (event: any) => {
+        UI.renderSettings(service.settingSelector());
+        console.log("TCL: EVENT", "easysubsSubtitlesChanged");
+        console.log("Language:", event.detail);
+
+        UI.renderSubs(service.playerContainerSelector());
+        UI.renderProgressBar(service.playerContainerSelector());
+        UI.renderNotifications();
+        service.getSubs(event.detail).then(subs => {
+          updateSubs(subs);
+        });
       });
     });
-  });
-  service.init();
+    service.init();
 
-  (subsStore as any).on(updateSubs, (state: subTitleType[], subs: subTitleType[]) => subs);
+    (subsStore as any).on(updateSubs, (state: subTitleType[], subs: subTitleType[]) => subs);
+  }
+} catch (e) {
+  Sentry.captureEvent(e);
 }
