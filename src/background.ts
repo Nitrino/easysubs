44c3a642cd3cb6cab945b961ecd1dec9156b7011
Petr Stepchenko <nitrinodev@gmail.com>
2020-12-12 02:21:57 +0300

    Fix full sub translation (#189)

--- src/background.ts
+++ src/background.ts
@@ -1,12 +1,47 @@
-import translate from "google-translate-open-api";
 import { stringify } from "querystring";
 import translateToken from "./token";
+import Utils from "./utils"
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  const baseUrl = "https://translate.google.com"
   if (request.contentScriptQuery === "translate") {
-    translate(request.text, { to: request.lang, raw: true })
-      .then((response: object) => {
-        sendResponse(response);
+    fetch(baseUrl)
+      .then(resp => resp.text())
+      .then(resp => {
+        console.log("bl", Utils.extract('cfb2h', resp));
+        console.log("f.sid", Utils.extract('FdrFJe', resp));
+        console.log("at", Utils.extract('SNlM0e', resp));
+
+        return {
+          query: {
+            'rpcids': 'MkEWBc',
+            'f.sid': Utils.extract('FdrFJe', resp),
+            'bl': Utils.extract('cfb2h', resp),
+            'hl': 'en',
+            'soc-app': 1,
+            'soc-platform': 1,
+            'soc-device': 1,
+            '_reqid': Math.floor(1000 + (Math.random() * 9000)),
+            'rt': 'c'
+          },
+          at: Utils.extract('SNlM0e', resp)
+        };
+      })
+      .then(data => {
+        const url = baseUrl + '/_/TranslateWebserverUi/data/batchexecute?' + stringify(data.query);
+        const payload = JSON.stringify([[request.text.replace(/(\r\n|\n|\r)/gm, ""), "auto", request.lang, true]])
+        const req = JSON.stringify([[['MkEWBc', payload, null, 'generic']]])
+        const body = 'f.req=' + encodeURIComponent(req) + '&at=' + data.at
+        const headers = { 'content-type': 'application/x-www-form-urlencoded;charset=UTF-8' }
+
+        fetch(url, {
+          method: 'POST',
+          body: body,
+          headers: headers
+        })
+          .then(resp => resp.text())
+          .then((resp: any) => Utils.getTranslateFromGoogleResponse(resp))
+          .then(respData => sendResponse(respData));
       })
       .catch((err: Error) => console.error(err));
   } else if (request.contentScriptQuery === "getSingleTranslation") {
