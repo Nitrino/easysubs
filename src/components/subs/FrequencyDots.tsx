c4ef2335f4b7b2dce349583f11bcf51360304d8f
Petr Stepchenko <nitrinodev@gmail.com>
2021-02-08 01:33:58 +0300

    DRAFT: Fix single word translation

--- src/components/subs/FrequencyDots.tsx
+++ src/components/subs/FrequencyDots.tsx
@@ -1,16 +1,14 @@
 import React from "react";
 
 function FrequencyDots(props: { frequency: number }): any {
-  const rate = props.frequency * 10000;
-
-  if (rate >= 500) {
+  if (props.frequency == 1) {
     return [
       <div key="1" className="easysubs-translate-alternative-item-frequency-dot -fill" />,
       <div key="2" className="easysubs-translate-alternative-item-frequency-dot -fill" />,
       <div key="3" className="easysubs-translate-alternative-item-frequency-dot -fill" />
     ];
   }
-  if (rate < 500 && rate >= 30) {
+  if (props.frequency == 2) {
     return [
       <div key="1" className="easysubs-translate-alternative-item-frequency-dot -fill" />,
       <div key="2" className="easysubs-translate-alternative-item-frequency-dot -fill" />,
