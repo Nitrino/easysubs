211d596df17a9e199fc7f304b38db75c3c996c56
Petr Stepchenko <nitrinodev@gmail.com>
2021-02-08 19:44:34 +0300

    Simplify frequency dots logic

--- src/components/subs/FrequencyDots.tsx
+++ src/components/subs/FrequencyDots.tsx
@@ -1,24 +1,11 @@
 import React from "react";
 
 function FrequencyDots(props: { frequency: number }): any {
-  if (props.frequency == 1) {
-    return [
-      <div key="1" className="easysubs-translate-alternative-item-frequency-dot -fill" />,
-      <div key="2" className="easysubs-translate-alternative-item-frequency-dot -fill" />,
-      <div key="3" className="easysubs-translate-alternative-item-frequency-dot -fill" />
-    ];
-  }
-  if (props.frequency == 2) {
-    return [
-      <div key="1" className="easysubs-translate-alternative-item-frequency-dot -fill" />,
-      <div key="2" className="easysubs-translate-alternative-item-frequency-dot -fill" />,
-      <div key="3" className="easysubs-translate-alternative-item-frequency-dot -empty" />
-    ];
-  }
+  const { frequency } = props;
   return [
     <div key="1" className="easysubs-translate-alternative-item-frequency-dot -fill" />,
-    <div key="2" className="easysubs-translate-alternative-item-frequency-dot -empty" />,
-    <div key="3" className="easysubs-translate-alternative-item-frequency-dot -empty" />
+    <div key="2" className={"easysubs-translate-alternative-item-frequency-dot " + (frequency < 3 ? "-fill" : '')} />,
+    <div key="3" className={"easysubs-translate-alternative-item-frequency-dot " + (frequency < 2 ? "-fill" : '')} />
   ];
 }
 
