6c348ab29c76c9607d0f5cc95721ce37fb2df8fe
Petr Stepchenko <nitrinodev@gmail.com>
2022-03-17 20:55:37 +0300

    Refactoring inject.ts

--- src/inject.ts
+++ src/inject.ts
@@ -4,7 +4,8 @@ import { updateSubs } from './event'
 import { subsStore } from './store'
 import UI from './ui'
 import Utils from './utils'
-;(Sentry as any).init({
+
+Sentry.init({
   dsn: 'https://f0696dfa1f80424f9f0f628d8d1d7796@sentry.io/1849876',
   defaultIntegrations: Sentry.defaultIntegrations.filter(({ name }: any) => name !== 'TryCatch'),
 })
@@ -15,30 +16,35 @@ window.addEventListener('unhandledrejection', (event) => {
 
 try {
   const service = Utils.detectService()
+
   if (service) {
-    console.log('Easysubs initialized. Service:', service.constructor.name)
-    window.addEventListener('easysubsVideoReady', () => {
-      console.log('TCL: EVENT', 'easysubsVideoReady')
+    const handleEasysubsSubtitlesChanged = (event: CustomEvent) => {
+      console.log('Event:', 'easysubsSubtitlesChanged')
+      console.log('Language:', event.detail)
+
+      UI.renderSubs(service.playerContainerSelector())
+      UI.renderProgressBar(service.playerContainerSelector())
+      UI.renderNotifications()
+      service.getSubs(event.detail).then((subs) => {
+        updateSubs(subs)
+      })
+    }
+
+    const handleEasysubsVideoReady = () => {
+      console.log('Event:', 'easysubsVideoReady')
 
       window.addEventListener('easysubsRenderSettings', () => {
         UI.renderSettings(service.settingsSelector(), service.settingsContentSelector())
       })
+    }
 
-      window.addEventListener('easysubsSubtitlesChanged', (event: any) => {
-        console.log('TCL: EVENT', 'easysubsSubtitlesChanged')
+    console.log('Easysubs initialized. Service:', service.constructor.name)
 
-        console.log('Language:', event.detail)
-
-        UI.renderSubs(service.playerContainerSelector())
-        UI.renderProgressBar(service.playerContainerSelector())
-        UI.renderNotifications()
-        service.getSubs(event.detail).then((subs) => {
-          updateSubs(subs)
-        })
-      })
-    })
+    window.addEventListener('easysubsVideoReady', handleEasysubsVideoReady)
+    window.addEventListener('easysubsSubtitlesChanged', handleEasysubsSubtitlesChanged as EventListener)
     service.init()
-    ;(subsStore as any).on(updateSubs, (state: subTitleType[], subs: subTitleType[]) => subs)
+
+    subsStore.on(updateSubs, (_, subs: subTitleType[]) => subs)
   }
 } catch (e) {
   console.error('Easysubs unknown error:', e)
