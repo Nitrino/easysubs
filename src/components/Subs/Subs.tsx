52331c37279904b2b102e43ad737d143a8054a96
Petr Stepchenko <petr@statice.io>
2022-09-13 01:23:44 +0300

    Pause video on hover

--- src/components/Subs/Subs.tsx
+++ src/components/Subs/Subs.tsx
@@ -2,6 +2,7 @@ import { Component, For, createSignal, Show, onMount } from 'solid-js'
 import { useUnit } from 'effector-solid'
 
 import { $currentSubs } from '@/models/subs'
+import { $video } from '@/models/videos'
 import { TSub, TSubItem } from '@/models/subs/types'
 import { getWordTranslationFx } from '@/models/translations'
 import { TWordTranslate } from '@/utils/googleTranslateFetcher'
@@ -19,9 +20,39 @@ const SubItemTranslation: Component<{ text: string }> = (props) => {
     </Show>
   )
 }
+const SubDescription: Component<{ text: string }> = (props) => {
+  const [translation, setTranslation] = createSignal<TWordTranslate | null>()
+  onMount(async () => {
+    const data = await getWordTranslationFx({ text: props.text, lang: 'ru' })
+    setTranslation(data)
+  })
+
+  return (
+    <Show when={translation() !== null && translation() !== undefined}>
+      <div class="es-sub-item-translation">DESCRIPTION</div>
+    </Show>
+  )
+}
 
 const SubItem: Component<{ subItem: TSubItem }> = (props) => {
   const [showTranslation, setShowTranslation] = createSignal(false)
+  const [showDescription, setShowDescription] = createSignal(false)
+
+  const handleOnMouseEnter = () => {
+    setShowTranslation(true)
+    setShowDescription(false)
+  }
+
+  const handleOnMouseLeave = () => {
+    setShowTranslation(false)
+    setShowDescription(false)
+  }
+
+  const handleClick = () => {
+    setShowTranslation(false)
+    setShowDescription(true)
+  }
+
   return (
     <>
       <span class="es-sub-item">
@@ -29,11 +60,15 @@ const SubItem: Component<{ subItem: TSubItem }> = (props) => {
           // eslint-disable-next-line solid/no-innerhtml
           innerHTML={props.subItem.text}
           class={props.subItem.tag}
-          onMouseEnter={() => setShowTranslation(true)}
-          onMouseLeave={() => setShowTranslation(false)}
+          onMouseEnter={handleOnMouseEnter}
+          onMouseLeave={handleOnMouseLeave}
+          onClick={handleClick}
         />
         <Show when={showTranslation()}>
           <SubItemTranslation text={props.subItem.cleanedText} />
+        </Show>
+        <Show when={showDescription()}>
+          <SubDescription text={props.subItem.cleanedText} />
         </Show>
       </span>
       <pre class="es-sub-item-space"> </pre>
@@ -41,9 +76,9 @@ const SubItem: Component<{ subItem: TSubItem }> = (props) => {
   )
 }
 
-const Sub: Component<{ sub: TSub }> = (props) => {
+const Sub: Component<{ sub: TSub; video: HTMLVideoElement }> = (props) => {
   return (
-    <div class="es-sub">
+    <div class="es-sub" onMouseEnter={() => props.video.pause()} onMouseLeave={() => props.video.play()}>
       <For each={props.sub.items}>{(subItem) => <SubItem subItem={subItem} />}</For>
     </div>
   )
@@ -51,9 +86,10 @@ const Sub: Component<{ sub: TSub }> = (props) => {
 
 export const Subs: Component = () => {
   const subs = useUnit($currentSubs)
+  const video = useUnit($video)
   return (
     <div id="es-subs">
-      <For each={subs()}>{(sub) => <Sub sub={sub} />}</For>
+      <For each={subs()}>{(sub) => <Sub sub={sub} video={video()!} />}</For>
     </div>
   )
 }
