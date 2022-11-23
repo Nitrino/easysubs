0c03b07e9e98003240012f3cc998bd89b4310220
Petr Stepchenko <nitrinodev@gmail.com>
2022-11-24 00:25:09 +0200

    Add overlay and close button to settings

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -6,16 +6,22 @@ interface SettingsProps {
   contentContainer: HTMLElement | null
 }
 
-const SettingsContent: Component = () => {
+const SettingsContent: Component<{ onClose: () => void }> = (props) => {
   return (
-    <div class="es-settings-content">
-      <div class="es-settings-content__menu">
-        <div class="es-settings-content__menu__title">
-          <LogoColor />
-          Easysubs
+    <div class="es-settings__overlay">
+      <div class="es-settings-content">
+        <div class="es-settings-content__menu">
+          <div class="es-settings-content__menu__title">
+            <LogoColor />
+            Easysubs
+          </div>
+          <div class="es-settings-content__menu__items">
+            <div class="es-settings-content__menu__item">General settings</div>
+          </div>
         </div>
+        <div class="es-settings-content__main">CONTENT</div>
+        <div class="es-settings-content__close" onClick={() => props.onClose()} />
       </div>
-      <div class="es-settings-content__main">CONTENT</div>
     </div>
   )
 }
@@ -30,12 +36,12 @@ export const Settings: Component<SettingsProps> = () => {
   }
   return (
     <>
-      <div class="es-settings-icon" onClick={handleClick}>
+      <div class="es-settings-icon" onClick={handleClick} onKeyPress={handleClick}>
         <LogoMono />
       </div>
       <Show when={showSettings()}>
         <Portal>
-          <SettingsContent />
+          <SettingsContent onClose={() => setShowSettings(false)} />
         </Portal>
       </Show>
     </>
