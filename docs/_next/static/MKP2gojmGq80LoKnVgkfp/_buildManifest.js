f019b15e1a5255f4876131402e17ab7cc783eeac
Petr Stepchenko <nitrinodev@gmail.com>
2025-07-31 17:56:30 +0200

    Revert "Deploy temporary version of the site"
    This reverts commit 5acc4344736cbc877639cacd91b234fbd5805c7b.

---
+++ docs/_next/static/MKP2gojmGq80LoKnVgkfp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-e6f4f1464cae7aba.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
